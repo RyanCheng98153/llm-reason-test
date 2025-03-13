910c74ec28359ff99b925ddffcbdfa58911c959a
ryan98153 <110703007@nccu.edu.tw>
2025-03-13 17:26:46 +0800

    test

--- deepseek.py
+++ deepseek.py
@@ -86,9 +86,9 @@ def generate_recap_aime2024(text_generator):
     
     # write aim2024 recaption to csv file
     with open("aime2024_recaption.csv", "w") as csvfile:
-        fieldnames = ["ID", "Problem", "Recap Process", "Recap"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+        fieldnames = ["ID", "Problem", "Generated Text", "Answer", "Recap Process", "Recap"]
+        writer = csv.writer(csvfile)
+        writer.writerows(fieldnames)
         
         for i, data in enumerate(dataset):
             print(f"Processing {i+1}/{len(dataset)}: {data['ID']}")
@@ -106,7 +106,7 @@ def generate_recap_aime2024(text_generator):
                 "Generated Text": recap_result["generated_text"],
                 "Recap Process": recap_result["recap_process"],
                 "Recap": recap_result["recap"],
-                # "Answer": answer
+                "Answer": answer
             })
 
 def evaluate_aime2024():
