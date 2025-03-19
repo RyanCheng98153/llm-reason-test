305d06b8ab80b55535aae3ac654e80eb827e194c
ryan98153 <110703007@nccu.edu.tw>
2025-03-20 00:57:06 +0800

    feat: write csv file write dict

--- deepseek.py
+++ deepseek.py
@@ -86,9 +86,12 @@ def generate_recap_aime2024(text_generator):
     
     # write aim2024 recaption to csv file
     with open("aime2024_recaption.csv", "w") as csvfile:
-        fieldnames = ["ID", "Problem", "Generated Text", "Answer", "Recap Process", "Recap"]
-        writer = csv.writer(csvfile)
-        writer.writerow(fieldnames)
+        titles = ["ID", "Problem", "Generated Text", "Answer", "Recap Process", "Recap", "Answer", "Solution"]
+        # writer = csv.writer(csvfile)
+        # writer.writerow(titles)
+        
+        writer = csv.DictWriter(csvfile, titles)
+        writer.writeheader()
         
         for i, data in enumerate(dataset):
             print(f"Processing {i+1}/{len(dataset)}: {data['ID']}")
@@ -106,7 +109,9 @@ def generate_recap_aime2024(text_generator):
                 "Generated Text": recap_result["generated_text"],
                 "Recap Process": recap_result["recap_process"],
                 "Recap": recap_result["recap"],
-                "Answer": recap_result["Answer"]
+                "Answer": recap_result["Answer"],
+                "Answer": answer,
+                "Solution": solution
             })
 
 def evaluate_aime2024():
