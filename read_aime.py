a2bb8c17b6009acbf1aac448ba484f1748fac430
ryan98153 <110703007@nccu.edu.tw>
2025-03-20 12:33:53 +0800

    test

--- read_aime.py
+++ read_aime.py
@@ -2,17 +2,19 @@ import sys
 import csv
 
 def read_aime2024_recaption(line: int):
-    with open("aime2024_recaption.csv", "r", encoding='utf-8') as csvfile:
+    with open("aime2024_recaption.csv", "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for i, row in enumerate(reader):
             if i == line:
-                print(f"[ ID ]: {row['ID']}")
-                print(f"[ Problem ]: {row['Problem']}")
-                print(f"[ Generated Text ]: {row['Generated Text']}")
-                print(f"[ Recap Process ]: {row['Recap Process']}")
-                print(f"[ Recap ]: {row['Recap']}")
-                print(f"[ Answer ]: {row['Answer']}")
-                print(f"[ Solution ]: {row['Solution']}")
+                print(f"[ ID ]: \n{row['ID']}")
+                print(f"[ Problem ]: \n{row['Problem']}")
+                print(f"[ Ground Truth ]: \n{row['Ground Truth']}")
+                print(f"[ Recap Process ]: \n{row['Recap Process']}")
+                print(f"[ Recap ]: \n{row['Recap']}")
+                print(f"[ Recap Answer ]: \n{row['Answer']}")
+                print(f"[ Prompt ]: \n{row['Prompt']}")
+                print(f"[ Generated Text ]: \n{row['Generated Text']}")
+                print(f"[ Solution ]: \n{row['Solution']}")
                 break
 
 if __name__ == "__main__":
